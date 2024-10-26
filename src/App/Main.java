a8fc3349b3455964830f2f455626c857a2ace0a2
yonshii <dkaranets2@gmail.com>
2024-10-26 15:05:12 +0200

    Implemented miles to kilometers conversion

--- src/App/Main.java
+++ src/App/Main.java
@@ -3,5 +3,11 @@ package App;
 public class Main {
     public static void main(String[] args) {
         System.out.println("App Converter");
+        double miles = 14;
+        double km = convMilesToKm(miles);
+        System.out.println(miles + " миль = " + km + " км");
+    }
+    private static double convMilesToKm(double miles) {
+        return miles * 1.60934;
     }
 }
